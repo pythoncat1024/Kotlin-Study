5f45abf0eef64029dd06fe205e0afa9a8cf8381f
pythonCat <pythoncat@qq.com>
2019-09-24 23:34:08 +0800

    kt 的属性委托与 java 的 PropertyChangeSupport

---
+++ src/cat/java/by/Main.java
@@ -0,0 +1,29 @@
+package cat.java.by;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Case cc = new Case();
+
+        cc.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                Object oldValue = evt.getOldValue();
+                Object newValue = evt.getNewValue();
+                String propertyName = evt.getPropertyName();
+                Object propagationId = evt.getPropagationId();
+                System.err.println(propagationId + "," + propertyName + "," + oldValue + "," + newValue);
+                cc.removePropertyChangeListener(this);
+            }
+        });
+        cc.setCaseName("case1");
+        cc.setCaseLevel(11);
+
+        System.out.println(cc);
+
+        cc.setCaseLevel(9);
+    }
+}
---
+++ src/cat/kt22.kt
@@ -0,0 +1,39 @@
+package cat
+
+import kotlin.reflect.KProperty
+
+class Example {
+    var ppt: String by Delegate()
+}
+
+class Delegate {
+
+    private var hideStr: String = "init"
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        System.err.println("get: $thisRef, thank you for delegating '${property.name}' to me!")
+        return hideStr
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        System.err.println("set: '$value' has been assigned to '${property.name} in $thisRef.'")
+        hideStr = value
+    }
+}
+
+
+fun main() {
+    val example = Example()
+
+
+    println("before set: ${example.ppt}")
+    example.ppt = "set from main"
+    println("after set: ${example.ppt}")
+
+    println("again: ${example.ppt}")
+
+    println("------------------------")
+
+    example.ppt = "set again"
+
+    println("set again:" + example.ppt)
+}
