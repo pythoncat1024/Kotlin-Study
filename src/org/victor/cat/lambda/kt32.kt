a3d30ed0c1949103c453dd455f287956675b078d
pythonCat <pythoncat@qq.com>
2019-10-06 10:02:52 +0800

    普通 lambda 与 带接收者的 lambda

--- src/org/victor/cat/lambda/kt32.kt
+++ src/org/victor/cat/lambda/kt32.kt
@@ -1,41 +1,45 @@
 package org.victor.cat.lambda
 
-import java.lang.Appendable
-
-
 fun main() {
 
-    isA<String>("a")
-    isA<String>(666)
-}
 
-//fun <T> isA(t:Any) = t is T  // 编译报错
-
-// 内联函数的泛型实化 - 非内联函数不能做到
-inline fun <reified T> isA(t: Any) = t is T // 可以编译运行
-
-
-fun <T> ensureTailingPeriod(seq: T) where T : CharSequence, T : Appendable {
-    if (!seq.endsWith('.')) {
-        seq.append('.')
+    val str = buildString {
+        append("Hello ")
+        append("world !")
     }
-}
+    println("str = $str")
 
-fun checkInList(value: Any) {
-    if (value is List<*>) {
-        // 依然不知道 List 的具体参数类型
+    val str1 = buildStr {
+        it.append("hello ").append("kotlin .")
     }
+
+    println("str1 = $str1")
+
+    println("buildStr1 = ${buildStr1 { it.append("hello ").append("duck @") }}")
 }
 
-//fun <T : Number> List<T>.sum(): T {
-//    var t: T
-//    for ((i, e) in this.withIndex()) {
-//        if (i == 0) {
-//            t = e
-//        } else {
-//            t = t + e
-//        }
-//    }
-//
-//    return t;
-//}
+/**
+ * 带接收者的 lambda
+ */
+// action: obj.() -> obj1
+// String. (Int , Int) -> Unit  -- 带接收者的 lambda
+fun buildString(builderAction: StringBuffer.() -> StringBuffer): String {
+
+    val sb = StringBuffer()
+    return sb.builderAction().toString()
+}
+
+/**
+ * 普通 lambda
+ */
+// action: (obj) -> obj1
+fun buildStr1(builderAction: (StringBuffer) -> StringBuffer): String {
+    val sb = StringBuffer()
+    return builderAction(sb).toString()
+}
+
+fun buildStr(builderAction: (StringBuffer) -> Unit): String {
+    val sb = StringBuffer()
+    builderAction(sb)
+    return sb.toString()
+}
