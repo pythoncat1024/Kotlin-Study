d12246f653f00b546c2c238b90824c753353a82f
pythonCat <pythoncat@qq.com>
2019-10-07 10:40:37 +0800

    导入 kotlinx-html-jvm ok
    按照 https://github.com/kotlin/kotlinx.html/wiki/Getting-started
    应该是在 pom.xml 里面添加

--- src/main/kotlin/org/victor/cat/Hello.kt
+++ src/main/kotlin/org/victor/cat/Hello.kt
@@ -1,6 +1,19 @@
 package org.victor.cat
 
+import kotlinx.html.div
+import kotlinx.html.li
+import kotlinx.html.ol
+import kotlinx.html.stream.appendHTML
+
+
 fun main(args: Array<String>) {
     println("Hello, World")
+
+    System.out.appendHTML()
+        .div {
+            ol {
+                li { +"1234" }
+            }
+        }
 }
 
