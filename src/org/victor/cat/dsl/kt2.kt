c34a67fb71becd35819fe1fe75e10215383e3a12
pythonCat <pythoncat@qq.com>
2019-10-07 09:00:12 +0800

    一步一步实现 dsl 之 step 4

--- src/org/victor/cat/dsl/kt2.kt
+++ src/org/victor/cat/dsl/kt2.kt
@@ -3,20 +3,19 @@ package org.victor.cat.dsl
 
 fun main() {
 
-    val div = DIV()
-    div.ol { obj: OL ->
-        obj
-            .li { e: LI ->
-                e.plus("hello")
-            }
-            .li { e: LI ->
-                e.plus("world")
-            }
-            .li { it + "duck" }
-    }
-        .ol { it.li { } }
-    println(div)
+    println(createDiv())
 }
+
+private fun createDiv() = DIV()
+    .ol {
+        li { content("java") }
+        li { content("kotlin") }
+    }
+    .ol {
+        li { content("apple") }
+        li { content("banana") }
+        li { content("orange") }
+    }
 
 abstract class DOM(val name: String) {
     var text: String = ""
@@ -30,14 +29,14 @@ abstract class DOM(val name: String) {
 
 class LI : DOM("li") {
 
-    operator fun plus(str: String) {
+    fun content(str: String) {
         this.text += str
     }
 
 }
 
 class OL : DOM("ol") {
-    fun li(init: (LI) -> Unit): OL {
+    fun li(init: LI.() -> Unit): OL {
         val li = LI()
         init(li)
         children.add(li)
@@ -47,10 +46,10 @@ class OL : DOM("ol") {
 
 class DIV : DOM("div") {
 
-    fun ol(init: (OL) -> Unit): DIV {
+    fun ol(init: OL.() -> Unit): DIV {
         val ol = OL()
         children.add(ol)
-        init(ol)
+        ol.init()
         return this
     }
 
