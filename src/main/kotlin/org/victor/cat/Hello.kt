32ecba477e0305dc3a3d7c47d5f497eb8f1bf89b
pythonCat <pythoncat@qq.com>
2019-10-07 10:02:51 +0800

    init a maven kotlin project ok

---
+++ src/main/kotlin/org/victor/cat/Hello.kt
@@ -0,0 +1,6 @@
+package org.victor.cat
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/org/victor/cat/HelloTest.kt
@@ -0,0 +1,8 @@
+package org.victor.cat
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
